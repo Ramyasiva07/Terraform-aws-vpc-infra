abca9621b5bfe2ab87225ecc61dca634d2f3667e
Ramyasiva07 <ramyasiva058@gmail.com>
2026-05-04 14:51:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,13 @@ resource "aws_subnet" "public" {
   map_public_ip_on_launch = true
 }
 
+resource "aws_subnet" "public2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = true
+}
+
 resource "aws_subnet" "private" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
